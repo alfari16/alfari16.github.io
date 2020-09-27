c7a8c890b6bc0e2cec7841aa1e9bbb314fd50183
Alif Irfan Anshory <alif.anshory@yahoo.com>
2020-09-27 22:03:01 +0700

    Update Works.67cf31db.chunk.js

--- static/js/Works.67cf31db.chunk.js
+++ static/js/Works.67cf31db.chunk.js
@@ -38,7 +38,13 @@ webpackJsonp([1],{82:function(e,n,o){"use strict";Object.defineProperty(n,"__esM
   {
     office: 'Fullstack Web Developer',
     position: 'Deliv Indonesia',
-    duration: 'September 2019 - now',
+    duration: 'September 2019 - September 2020',
+    icon: 'fa-briefcase'
+  },
+  {
+    office: 'Software Engineer',
+    position: 'eFishery',
+    duration: 'September 2020 - present',
     icon: 'fa-briefcase'
   },
 ].map(function(e,n){return i.a.createElement("div",{className:"single-job row py-4",key:n},i.a.createElement("div",{className:"col-2"},i.a.createElement("i",{className:"fa icon " + e.icon})),i.a.createElement("div",{className:"col-10"},i.a.createElement("h4",{className:"title"},e.office),i.a.createElement("p",{className:"desc"},e.position),i.a.createElement("p",{className:"desc"},e.duration)))});n.default=function(){return i.a.createElement(t.Fragment,null,i.a.createElement(r,{bgColor:"#744F81",color:"white",icon:"fa-briefcase"},"Experiences"),i.a.createElement("div",{className:"item-container"},i.a.createElement("div",{className:"container"},c)))}},95:function(e,n,o){var t=o(96);"string"===typeof t&&(t=[[e.i,t,""]]);var i={hmr:!1};i.transform=void 0;o(78)(t,i);t.locals&&(e.exports=t.locals)},96:function(e,n,o){n=e.exports=o(77)(!0),n.push([e.i,".single-job:not(:last-child){border-bottom:1px solid #eee}.single-job{padding:10px 0}.single-job .icon{font-size:40px}.single-job .title{font-weight:900;font-size:20px}.single-job .desc{margin-bottom:0;color:#666}","",{version:3,sources:["/home/alfari/Documents/new-portfolio/src/components/Works/Works.css"],names:[],mappings:"AAAA,6BACE,4BAA8B,CAC/B,AAED,YACE,cAAgB,CACjB,AAED,kBACE,cAAgB,CACjB,AAED,mBACE,gBAAiB,AACjB,cAAgB,CACjB,AAED,kBACE,gBAAiB,AACjB,UAAY,CACb",file:"Works.css",sourcesContent:[".single-job:not(:last-child) {\n  border-bottom: 1px solid #eee;\n}\n\n.single-job {\n  padding: 10px 0;\n}\n\n.single-job .icon {\n  font-size: 40px;\n}\n\n.single-job .title {\n  font-weight: 900;\n  font-size: 20px;\n}\n\n.single-job .desc {\n  margin-bottom: 0;\n  color: #666;\n}\n"],sourceRoot:""}])}});
